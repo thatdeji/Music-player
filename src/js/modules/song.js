127aa70f78a7948587bc711687265d7f288e6228
Akindeji <akindejioluwaseun015@gmail.com>
2021-04-17 05:38:58 -0700

    added music list

---
+++ src/js/modules/song.js
@@ -0,0 +1,98 @@
+export const songList = [
+  {
+    title: "Element",
+    artist: "Kendrick Lamar",
+    url: "music/Kendrick/ELEMENT",
+    cover: "music/Kendrick/cover"
+  },
+  {
+    title: "Shea Butter Baby",
+    artist: "Ari Lennox ft. J.cole",
+    url: "music/J.cole/Shea Butter Baby",
+    cover: "music/J.cole/cover"
+  },
+  {
+    title: "Wishing Well",
+    artist: "Juice Wrld",
+    url: "music/Juice Wrld/Wishing Well",
+    cover: "music/Juice Wrld/cover"
+  },
+  {
+    title: "Topanga",
+    artist: "Trippie Redd",
+    url: "music/Trippie Redd/Topanga",
+    cover: "music/Trippie Redd/cover"
+  },
+  {
+    title: "NO BYSTANDERS",
+    artist: "Travis Scott",
+    url: "music/Travis Scott/NO BYSTANDERS",
+    cover: "music/Travis Scott/cover"
+  },
+  {
+    title: "Moonlight",
+    artist: "XXXtentacion",
+    url: "music/XXXtentacion/Moonlight",
+    cover: "music/XXXtentacion/cover"
+  },
+  {
+    title: "Gunna",
+    artist: "Gunna ft. Young thug & Wheezy",
+    url: "music/Gunna/3 Headed Snake",
+    cover: "music/Gunna/cover"
+  },
+  {
+    title: " Praise the Lord (Da Shine)",
+    artist: "A$AP Rocky ft. Skepta",
+    url: "music/A$AP/Praise the Lord (Da Shine)",
+    cover: "music/A$AP/cover"
+  },
+  {
+    title: "Facts",
+    artist: "Kanye West",
+    url: "music/Kanye West/Facts (Charlie Heat Version)",
+    cover: "music/Kanye West/Cover"
+  },
+  {
+    title: "Onyeka",
+    artist: "Burna boy",
+    url: "music/Burna boy/Onyeka",
+    cover: "music/Burna boy/cover"
+  },
+  {
+    title: "No More Parties",
+    artist: "Coi Leray",
+    url: "music/Coi Leray/No More Parties",
+    cover: "music/Coi Leray/cover"
+  },
+  {
+    title: "10 Freaky Girls",
+    artist: "Metro Boomin",
+    url: "music/Metro Boomin/10 Freaky Girls",
+    cover: "music/Metro Boomin/cover"
+  },
+  {
+    title: "Y.M.C.A.",
+    artist: "Village People",
+    url: "music/Village People/Y.M.C.A.",
+    cover: "music/Village People/cover"
+  },
+  {
+    title: "Nuketown",
+    artist: "Skimask the Slump God",
+    url: "music/Skimask/Nuketown",
+    cover: "music/Skimask/cover"
+  },
+  {
+    title: "Circle of bosses",
+    artist: "Young thug ft. Quavo",
+    url: "music/Young Thug/Circle of bosses",
+    cover: "music/Young Thug/cover"
+  },
+  {
+    title: "No Permission",
+    artist: "Nasty C ft. Runtown",
+    url: "music/Nasty C/No Permission",
+    cover: "music/Nasty C/cover"
+  }
+];
