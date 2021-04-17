76e28bd03dc6b0681283cfc3999d9a0d285ffdd7
Akindeji <akindejioluwaseun015@gmail.com>
2021-04-17 06:16:44 -0700

    made the playlist render function

--- src/js/modules/playlist.js
+++ src/js/modules/playlist.js
@@ -1,1 +1,25 @@
-const playList = (() => {})();
+import songList from "./song.js";
+
+const playList = (() => {
+  // caches the DOM
+  const musicListEl = document.querySelector("#js-music-list"),
+    menuBtn = document.querySelector("#js-menu-btn"),
+    sideBar = document.querySelector("#js-sidebar");
+  // renders to the DOM
+  const render = () => {
+    let markup = "";
+    songList.forEach((song, index) => {
+      markup += `
+    <li class = "music-list-item ${index === state.currentlyPlayingIndex
+      ? " is-active"
+      : ""}" id="${index}">
+      <img src= "${songList[index]
+        .cover}.jpg" alt="" class="music-list-image" id="${index}" />
+      <h6 class="music-list-title" id="${index}">${songList[index].title}</h6>
+    </li>`;
+    });
+    musicListEl.innerHTML = markup;
+  };
+})();
+
+export default playList;
