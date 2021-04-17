025b340c0dd5ec42fbc5f024a0eb3846189d98de
Akindeji <akindejioluwaseun015@gmail.com>
2021-04-17 06:03:53 -0700

    built the trackbar render function

--- src/js/modules/trackbar.js
+++ src/js/modules/trackbar.js
@@ -1,3 +1,31 @@
-const trackBar = () => {};
+// IIFE for trackbar module
+const trackBar = (() => {
+  // cache the DOM
+  const trackContainerEl = document.querySelector("#js-track-container"),
+    trackBarEl = document.querySelector("#js-track-bar");
+  //state
+  const state = {
+    currentTime: 0,
+    totalTime: 0,
+    fillWidth: 0
+  };
+  // renders to the DOM
+  const render = () => {
+    trackBarEl.style.width = `${trackbarState.fillWidth}%`;
+  };
+  // sets state and renders to the DOM
+  const setState = obj => {
+    const { duration, currentTime } = obj;
+    trackbarState.currentTime = currentTime;
+    trackbarState.totalTime = duration;
+    trackbarState.fillWidth =
+      trackbarState.currentTime / trackbarState.totalTime * 100;
+    render();
+  };
+  // initialize module
+  const init = () => {
+    render();
+  };
+})();
 
 export default trackBar;
