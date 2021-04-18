108a61a963b6c4f8948371da5a74ce38c2c46d2b
Akindeji <akindejioluwaseun015@gmail.com>
2021-04-17 17:57:46 -0700

    deleted dist files

--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -635,7 +635,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50210" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60089" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
